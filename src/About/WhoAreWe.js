acae4d840369b6e88983d07331c93157d569c072
Naveen Perera <naveenperera777@gmail.com>
2020-02-02 19:28:42 +0530

    Change about text

--- src/About/WhoAreWe.js
+++ src/About/WhoAreWe.js
@@ -14,11 +14,31 @@ export default function WhoAreWe(){
 								Who Are We?                                				
 							</h1>	
 							<p>
-                            Quia iusto totam facilis ut atque quidem quis maiores iure. Facilis mollitia enim illo sed et totam commodi. Velit a recusandae sequi consequatur est dolorum. Eaque accusantium praesentium ut omnis. Laboriosam reprehenderit commodi assumenda.
-                            </p>
+							It took 28 Failures to make 29 th our Success, so it began…………….
+							Venture29 is born out of passion to face the challenges of the modern day
+							society. Everyday is a new day and brings new challenges with it, our mission is
+							to find solutions to overcome these challenges by coming up with innovative
+							products and services that enhance and empower everyone around us.
+							</p>
                             <p>
-                            Quia iusto totam facilis ut atque quidem quis maiores iure. Facilis mollitia enim illo sed et totam commodi. Velit a recusandae sequi consequatur est dolorum. Eaque accusantium praesentium ut omnis. Laboriosam reprehenderit commodi assumenda.
-                            </p>                       
+							What makes us different from other Venture Capital Firms? Well, we build every
+							company from ground up and we don’t “follow the crowd” what we do is to
+							create Challenger Brands that questions the norm and focus our energy on
+							innovation.
+                            </p>  
+							<p>
+							Furthermore, our key focus is to invest in developing markets such as Africa and
+							Asia where we have high growth potential with large number of users.
+							All our investors share our Values and Ambitions to go beyond the expectations
+							and drive towards one goal. If you are an Investor who is daring and ready to
+							take on the World, Then you are in the right place for your next Big Investment!
+								</p>  
+							<p>
+								So far we have invested over $500 Million Dollars over 20 Projects and looking
+								to expand our global presence by investing into Challenger Brands all over the
+								word. So come and join us on this Venture and become part of something
+								Amazing!	
+								</p>		                   
 						</div>	
 					</div>
 				</div>
